9b16b327cf32f7b1b08a47e5504563bce5d79628
Zergba <nico.gylis@gmail.com>
2017-03-30 13:46:29 +0200

    completion de RessourceString.cs (Done !)

--- ABIMS/ABIMS/Form1.Designer.cs
+++ ABIMS/ABIMS/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(713, 561);
+            this.ClientSize = new System.Drawing.Size(248, 202);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
--- ABIMS/ABIMS/RessourcesString.cs
+++ ABIMS/ABIMS/RessourcesString.cs
@@ -30,7 +30,33 @@ namespace ABIMS
         public const String _strNature = "Nature";
         public const String _strSalesRevenu = "Chiffre d'affaire";
         public const String _strStaff = "Effectif";
+        public const String _strDetail = "Détail";
+        public const String _strSocietyName = "Raison sociale";
+        public const String _strAdress = "Adresse";
+        public const String _strPhone = "Téléphone";
+        public const String _strModification = "Modification";
+        public const String _strKeepOpen = "Garder ouvert";
+        public const String _strContacts = "Contacts";
+        public const String _strComm = "Commentaires";
+        public const String _strName = "Nom";
+        public const String _strFirsName = "Prénom";
+        public const String _strFunction = "Fonction";
+        public const String _strEmail = "Email";
+        public const String _strAddContact = "Ajouter contact";
+        public const String _strInterventionDuration = "Durée d'intervention";
+        public const String _strSupprimer = "le contact";
+        public const String _strModifyContact = "Modifier le contact";
+        public const String _strDeleteContact = "Supprimer le contact";
+        public const String _strValidModification = "Valider les modifications";
+        public const String _strCancelModification = "Annuler les modifications";
+        public const String _strDeleteClient = "Supprimer le client";
+        public const String _strDetailClient = "Détail Client";
+        public const String _strValidCreation = "Valider la creation";
+        public const String _strCancelCreation = "Annuler la création";
+        public const String _strSeeTheSheet = "Voir la fiche après la création";
         private const String _strClientList = "Liste de clients";
+
+
 
         public static string strClientList
         {
