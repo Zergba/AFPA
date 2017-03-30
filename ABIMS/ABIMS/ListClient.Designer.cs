2a8fe64fd2d6f4424461d1f5e07077f0954ab78f
Zergba <nico.gylis@gmail.com>
2017-03-30 14:20:34 +0200

    ajout du form listClient ajout du form ParentClient

---
+++ ABIMS/ABIMS/ListClient.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace ABIMS
+{
+    partial class ListClient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1029, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(125, 20);
+            this.toolStripMenuItem1.Text = "toolStripMenuItem1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.checkBox4);
+            this.groupBox1.Controls.Add(this.checkBox3);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 28);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 123);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Location = new System.Drawing.Point(13, 157);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 80);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button6);
+            this.groupBox3.Controls.Add(this.button5);
+            this.groupBox3.Controls.Add(this.button4);
+            this.groupBox3.Location = new System.Drawing.Point(13, 243);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 107);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "groupBox3";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.button8);
+            this.groupBox4.Controls.Add(this.button7);
+            this.groupBox4.Controls.Add(this.comboBox1);
+            this.groupBox4.Location = new System.Drawing.Point(13, 356);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(200, 107);
+            this.groupBox4.TabIndex = 4;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "groupBox4";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(7, 20);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(94, 20);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(80, 17);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(7, 44);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(80, 17);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "checkBox3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(94, 44);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(80, 17);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "checkBox4";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(7, 68);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(187, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(7, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(187, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(7, 20);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(187, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(7, 50);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(187, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(7, 20);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(187, 23);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(7, 50);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(187, 23);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(7, 79);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(187, 23);
+            this.button6.TabIndex = 2;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(7, 20);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(187, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(7, 48);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(187, 23);
+            this.button7.TabIndex = 1;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(7, 78);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(187, 23);
+            this.button8.TabIndex = 2;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(220, 28);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(797, 527);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // ListClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1029, 634);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "ListClient";
+            this.Text = "ListClient";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
--- ABIMS/ABIMS/RessourcesString.cs
+++ ABIMS/ABIMS/RessourcesString.cs
@@ -8,61 +8,429 @@ namespace ABIMS
 {
     class RessourcesString
     {
-        public const String _strTitle = "ABIMS - ";
-        public const String _strSearch = "Rechercher";
-        public const String _strSearchSentence = "Saisir les termes de votre recherche";
-        public const String _strNumber = "Numéro";
-        public const String _strClient = "Client";
-        public const String _strContact = "Contact";
-        public const String _strProject = "Projet";
-        public const String _strClientMgt = "Gestion client";
-        public const String _strAddClient = "Ajouter un client";
-        public const String _strSeeClients = "Voir les clients";
-        public const String _strDeleteClients = "Supprimer les clients";
-        public const String _strWindows = "Fenêtres";
-        public const String _strReopenClient = "Réouvrir le client";
-        public const String _strCloseWindows = "Fermer toutes les fenêtres";
-        public const String _strList = "Liste";
-        public const String _strShowList = "Afficher liste";
-        public const String _strDropList = "Vider liste";
-        public const String _strType = "Type";
-        public const String _strActivityDomain = "Domaine d'activité";
-        public const String _strNature = "Nature";
-        public const String _strSalesRevenu = "Chiffre d'affaire";
-        public const String _strStaff = "Effectif";
-        public const String _strDetail = "Détail";
-        public const String _strSocietyName = "Raison sociale";
-        public const String _strAdress = "Adresse";
-        public const String _strPhone = "Téléphone";
-        public const String _strModification = "Modification";
-        public const String _strKeepOpen = "Garder ouvert";
-        public const String _strContacts = "Contacts";
-        public const String _strComm = "Commentaires";
-        public const String _strName = "Nom";
-        public const String _strFirsName = "Prénom";
-        public const String _strFunction = "Fonction";
-        public const String _strEmail = "Email";
-        public const String _strAddContact = "Ajouter contact";
-        public const String _strInterventionDuration = "Durée d'intervention";
-        public const String _strSupprimer = "le contact";
-        public const String _strModifyContact = "Modifier le contact";
-        public const String _strDeleteContact = "Supprimer le contact";
-        public const String _strValidModification = "Valider les modifications";
-        public const String _strCancelModification = "Annuler les modifications";
-        public const String _strDeleteClient = "Supprimer le client";
-        public const String _strDetailClient = "Détail Client";
-        public const String _strValidCreation = "Valider la creation";
-        public const String _strCancelCreation = "Annuler la création";
-        public const String _strSeeTheSheet = "Voir la fiche après la création";
-        private const String _strClientList = "Liste de clients";
+        private const String strTitle = "ABIMS - ";
+        private const String strSearch = "Rechercher";
+        private const String strSearchSentence = "Saisir les termes de votre recherche";
+        private const String strNumber = "Numéro";
+        private const String strClient = "Client";
+        private const String strContact = "Contact";
+        private const String strProject = "Projet";
+        private const String strClientMgt = "Gestion client";
+        private const String strAddClient = "Ajouter un client";
+        private const String strSeeClients = "Voir les clients";
+        private const String strDeleteClients = "Supprimer les clients";
+        private const String strWindows = "Fenêtres";
+        private const String strReopenClient = "Réouvrir le client";
+        private const String strCloseWindows = "Fermer toutes les fenêtres";
+        private const String strList = "Liste";
+        private const String strShowList = "Afficher liste";
+        private const String strDropList = "Vider liste";
+        private const String strType = "Type";
+        private const String strActivityDomain = "Domaine d'activité";
+        private const String strNature = "Nature";
+        private const String strSalesRevenu = "Chiffre d'affaire";
+        private const String strStaff = "Effectif";
+        private const String strDetail = "Détail";
+        private const String strSocietyName = "Raison sociale";
+        private const String strAdress = "Adresse";
+        private const String strPhone = "Téléphone";
+        private const String strModification = "Modification";
+        private const String strKeepOpen = "Garder ouvert";
+        private const String strContacts = "Contacts";
+        private const String strComm = "Commentaires";
+        private const String strName = "Nom";
+        private const String strFirsName = "Prénom";
+        private const String strFunction = "Fonction";
+        private const String strEmail = "Email";
+        private const String strAddContact = "Ajouter contact";
+        private const String strInterventionDuration = "Durée d'intervention";
+        private const String strSupprimer = "le contact";
+        private const String strModifyContact = "Modifier le contact";
+        private const String strDeleteContact = "Supprimer le contact";
+        private const String strValidModification = "Valider les modifications";
+        private const String strCancelModification = "Annuler les modifications";
+        private const String strDeleteClient = "Supprimer le client";
+        private const String strDetailClient = "Détail Client";
+        private const String strValidCreation = "Valider la creation";
+        private const String strCancelCreation = "Annuler la création";
+        private const String strSeeTheSheet = "Voir la fiche après la création";
+        private const String strClientList = "Liste de clients";
 
 
 
-        public static string strClientList
+        public static string StrClientList
         {
             get
             {
-                return _strClientList;
+                return strClientList;
+            }
+        }
+
+        public static string StrTitle
+        {
+            get
+            {
+                return strTitle;
+            }
+        }
+
+        public static string StrSearch
+        {
+            get
+            {
+                return strSearch;
+            }
+        }
+
+        public static string StrSearchSentence
+        {
+            get
+            {
+                return strSearchSentence;
+            }
+        }
+
+        public static string StrNumber
+        {
+            get
+            {
+                return strNumber;
+            }
+        }
+
+        public static string StrClient
+        {
+            get
+            {
+                return strClient;
+            }
+        }
+
+        public static string StrContact
+        {
+            get
+            {
+                return strContact;
+            }
+        }
+
+        public static string StrProject
+        {
+            get
+            {
+                return strProject;
+            }
+        }
+
+        public static string StrClientMgt
+        {
+            get
+            {
+                return strClientMgt;
+            }
+        }
+
+        public static string StrAddClient
+        {
+            get
+            {
+                return strAddClient;
+            }
+        }
+
+        public static string StrSeeClients
+        {
+            get
+            {
+                return strSeeClients;
+            }
+        }
+
+        public static string StrDeleteClients
+        {
+            get
+            {
+                return strDeleteClients;
+            }
+        }
+
+        public static string StrWindows
+        {
+            get
+            {
+                return strWindows;
+            }
+        }
+
+        public static string StrReopenClient
+        {
+            get
+            {
+                return strReopenClient;
+            }
+        }
+
+        public static string StrCloseWindows
+        {
+            get
+            {
+                return strCloseWindows;
+            }
+        }
+
+        public static string StrList
+        {
+            get
+            {
+                return strList;
+            }
+        }
+
+        public static string StrShowList
+        {
+            get
+            {
+                return strShowList;
+            }
+        }
+
+        public static string StrDropList
+        {
+            get
+            {
+                return strDropList;
+            }
+        }
+
+        public static string StrType
+        {
+            get
+            {
+                return strType;
+            }
+        }
+
+        public static string StrActivityDomain
+        {
+            get
+            {
+                return strActivityDomain;
+            }
+        }
+
+        public static string StrNature
+        {
+            get
+            {
+                return strNature;
+            }
+        }
+
+        public static string StrSalesRevenu
+        {
+            get
+            {
+                return strSalesRevenu;
+            }
+        }
+
+        public static string StrStaff
+        {
+            get
+            {
+                return strStaff;
+            }
+        }
+
+        public static string StrDetail
+        {
+            get
+            {
+                return strDetail;
+            }
+        }
+
+        public static string StrSocietyName
+        {
+            get
+            {
+                return strSocietyName;
+            }
+        }
+
+        public static string StrAdress
+        {
+            get
+            {
+                return strAdress;
+            }
+        }
+
+        public static string StrPhone
+        {
+            get
+            {
+                return strPhone;
+            }
+        }
+
+        public static string StrModification
+        {
+            get
+            {
+                return strModification;
+            }
+        }
+
+        public static string StrKeepOpen
+        {
+            get
+            {
+                return strKeepOpen;
+            }
+        }
+
+        public static string StrContacts
+        {
+            get
+            {
+                return strContacts;
+            }
+        }
+
+        public static string StrComm
+        {
+            get
+            {
+                return strComm;
+            }
+        }
+
+        public static string StrName
+        {
+            get
+            {
+                return strName;
+            }
+        }
+
+        public static string StrFirsName
+        {
+            get
+            {
+                return strFirsName;
+            }
+        }
+
+        public static string StrFunction
+        {
+            get
+            {
+                return strFunction;
+            }
+        }
+
+        public static string StrEmail
+        {
+            get
+            {
+                return strEmail;
+            }
+        }
+
+        public static string StrAddContact
+        {
+            get
+            {
+                return strAddContact;
+            }
+        }
+
+        public static string StrInterventionDuration
+        {
+            get
+            {
+                return strInterventionDuration;
+            }
+        }
+
+        public static string StrSupprimer
+        {
+            get
+            {
+                return strSupprimer;
+            }
+        }
+
+        public static string StrModifyContact
+        {
+            get
+            {
+                return strModifyContact;
+            }
+        }
+
+        public static string StrDeleteContact
+        {
+            get
+            {
+                return strDeleteContact;
+            }
+        }
+
+        public static string StrValidModification
+        {
+            get
+            {
+                return strValidModification;
+            }
+        }
+
+        public static string StrCancelModification
+        {
+            get
+            {
+                return strCancelModification;
+            }
+        }
+
+        public static string StrDeleteClient
+        {
+            get
+            {
+                return strDeleteClient;
+            }
+        }
+
+        public static string StrDetailClient
+        {
+            get
+            {
+                return strDetailClient;
+            }
+        }
+
+        public static string StrValidCreation
+        {
+            get
+            {
+                return strValidCreation;
+            }
+        }
+
+        public static string StrCancelCreation
+        {
+            get
+            {
+                return strCancelCreation;
+            }
+        }
+
+        public static string StrSeeTheSheet
+        {
+            get
+            {
+                return strSeeTheSheet;
             }
         }
     }
