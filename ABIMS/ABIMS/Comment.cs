d2c61815fe1b620b0604a4df3903a1be73c22957
Zergba <nico.gylis@gmail.com>
2017-04-03 09:54:21 +0200

    ajout des classes metier contact et commentaire

---
+++ ABIMS/ABIMS/Comment.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABIMS
+{
+    class Comment
+    {
+        private String author;
+        private DateTime date;
+        private String text;
+
+        public string Author
+        {
+            get
+            {
+                return author;
+            }
+
+            set
+            {
+                author = value;
+            }
+        }
+
+        public DateTime Date
+        {
+            get
+            {
+                return date;
+            }
+
+            set
+            {
+                date = value;
+            }
+        }
+
+        public string Text
+        {
+            get
+            {
+                return text;
+            }
+
+            set
+            {
+                text = value;
+            }
+        }
+    }
+}
---
+++ ABIMS/ABIMS/Contact.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABIMS
+{
+    class Contact
+    {
+        String name;
+        String phone;
+        String email;
+        Int32 totalTimeWorked;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string Phone
+        {
+            get
+            {
+                return phone;
+            }
+
+            set
+            {
+                phone = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+
+            set
+            {
+                email = value;
+            }
+        }
+
+        public int TotalTimeWorked
+        {
+            get
+            {
+                return totalTimeWorked;
+            }
+
+            set
+            {
+                totalTimeWorked = value;
+            }
+        }
+    }
+}
